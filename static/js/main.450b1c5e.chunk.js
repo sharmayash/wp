0c8aa77bf931452d6ba190e0bbbc4073f33ee287
Yash sharma <sharmayash44ys@gmail.com>
2021-03-07 18:26:49 +0530

    Updates

---
+++ static/js/main.450b1c5e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwp=this.webpackJsonpwp||[]).push([[0],{11:function(n,t,e){"use strict";e.r(t);e(1);var c=e(3),o=e.n(c),r=(e(8),e(9),e(0));var i=function(){return Object(r.jsx)("div",{children:Object(r.jsx)("h1",{children:"Porfolio"})})};o.a.render(Object(r.jsx)(i,{}),document.getElementById("root"))},8:function(n,t,e){},9:function(n,t,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.450b1c5e.chunk.js.map
